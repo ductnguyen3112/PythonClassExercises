9b57ab0cc7d5c8ca853e8d369641f96f4a4e90ef
ductnguyen3112 <ductnguyen3112@gmail.com>
2022-02-17 22:25:06 -0500

    Second commit

--- class01/exercise02.py
+++ class01/exercise02.py
@@ -1,25 +1,35 @@
 import time
-aws = int(input('Enter PIN:  '))
+def password_checker():
 
-if aws == 123:
+    correct_passwd = 41*3
+    print("Please Enter the PIN")
+    while True:
+        password = input("PIN:  ")
+        if password == correct_passwd:
+
+            print('Logged In, Welcome!')
+            time.sleep(1)
+            print("Loading...")
+            time.sleep(1)
+            print("Abit More..")
+            time.sleep(1)
+            print("Done!.")
+            time.sleep(1)
+            ######function start######
+            a = [3, 4, 5]
+            b = a[0] or a[1]
+            if b:
+                print('b is true')
+            else:
+                print('b is false')
+            break
+            #####function stop######
+        else:
+            print("Invalid PIN, please try again...")
+password_checker()
 
 
-    print('Logged In, Welcome!')
-    print("Loading....")
-    time.sleep(1)
-    print("Abit More...")
-    time.sleep(1)
-    print("Done!..")
-    time.sleep(1)
 
-    a = [3, 4, 5]
-    b = a[0] or a[1]
-    if b:
-        print('b is true')
-    else:
-        print('b is false')
-else:
-    print('Wrong Password')
 
 
 
